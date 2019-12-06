65bdbda2472eef726d17819212f4138ced17c64d
art <art@wifilapperhome>
2019-12-05 19:50:09 -0700

    adding compose.sh

---
+++ docker/compose.sh
@@ -0,0 +1,8 @@
+docker-compose -f docker-compose.yml \
+    -f pgsql/docker-compose.base.yml \
+    -f pgsql/docker-compose.meta.yml \
+    -f pgsql/docker-compose.event.yml \
+    -f pgsql/docker-compose.model.yml \
+    up
+export PATH=`pwd`/bin:$PATH
+pio-docker status
